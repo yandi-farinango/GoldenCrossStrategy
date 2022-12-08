2f6503950cbe211028f72ae9f25a1167629e630a
Yandi Farinango <71475457+yandi-farinango@users.noreply.github.com>
2022-12-08 14:20:55 -0600

    Added BuyHold

--- run.py
+++ run.py
@@ -9,6 +9,22 @@ import pandas as pd
 import backtrader as bt 
 from backtrader import Cerebro
 from strategies.GoldenCross import GoldenCross
+from strategies.BuyHold import BuyHold
+
+strategies = {
+    'golden_cross': GoldenCross,
+    'buy_hold': BuyHold, 
+}
+
+# Arguement parser to take strategies from command line 
+parser = argparse.ArgumentParser()
+parser.add_argument('strategy', help='Which Strategy to run', type=str)
+args = parser.parse_args()
+
+if not args.strategy in strategies:
+    print('Invalid strategy, must be one of {}'.format(strategies.keys()))
+    sys.exit()
+
 
 cerebro = Cerebro()
 cerebro.broker.setcash(100000)
@@ -20,7 +36,7 @@ feed = bt.feeds.PandasData(dataname=spy_prices)
 cerebro.adddata(feed)
 
 # Add strategy 
-cerebro.addstrategy(GoldenCross)
+cerebro.addstrategy(strategies[args.strategy])
 
 cerebro.run
 cerebro.plot()
---
+++ strategies/BuyHold.py
@@ -0,0 +1,14 @@
+"""
+Buy Hold Strategy 
+
+"""
+
+import backtrader as bt
+
+class BuyHold(bt.Strategy):
+    def next(self):
+        if self.position.size == 0:
+            size = int(self.broker.getcash() / self.data)
+            self.buy(size=size)
+
+        
